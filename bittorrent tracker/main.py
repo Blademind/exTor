b44e02a60472db4a552fb295151085138722d212
Blademind <alonlevy2005@gmail.com>
2022-10-22 15:40:05 +0300

    server supports torrent file recieve

--- bittorrent tracker/main.py
+++ bittorrent tracker/main.py
@@ -1,4 +1,5 @@
 import _thread
+import pickle
 import time
 from random import randbytes
 
@@ -15,28 +16,45 @@ import os
 
 class Tracker:
     def __init__(self):
-        self.init_sock()
+        self.server_sock2 = None  # tcp sock
+        self.server_sock = None  # udp sock
+        self.init_tcp_sock()
+        self.init_udp_sock()
         self.__BUF = 1024
-        self.read, self.write = [self.server_sock], []  # read write for select
+        self.read_udp, self.write_udp = [self.server_sock], []  # read write for select udp
+        self.read_tcp, self.write_tcp = [self.server_sock2], []  # read write for select udp
+
         self.connection_ids = {}  # list of all connected clients
         self.ip_addresses = [("192.168.1.196", 1234)]
-        self.listen()  # listen
+        _thread.start_new_thread(self.listen_tcp, ())
+        self.listen_udp()  # listen
 
-    def init_sock(self):
+    def init_udp_sock(self):
         self.server_sock = socket(AF_INET, SOCK_DGRAM)
         self.server_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.server_sock.bind(("0.0.0.0", 55555))
+        self.server_sock.bind(("192.168.1.196", 55555))
 
-    def listen(self):
+    def init_tcp_sock(self):
+        self.server_sock2 = socket(AF_INET, SOCK_STREAM)
+        self.server_sock2.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.server_sock2.bind(("192.168.1.196", 55556))
+
+    def listen_udp(self):
         """
         Listens to incoming communications
         """
         while 1:
-            readable, writeable, ex = select.select(self.read, self.write, [])
+            readable, writeable, ex = select.select(self.read_udp, self.write_udp, [])
             for sock in readable:
                 data, addr = sock.recvfrom(self.__BUF)
                 if not data:
                     break
+                try:
+                    datacontent = data.decode()
+                    if datacontent == "TCP_SERVER":
+                        sock.sendto(pickle.dumps(gethostbyname(gethostname())), addr)
+                except:
+                    pass
                 # request must be at least 16 bytes long
                 if len(data) >= 16:
                     try:
@@ -58,6 +76,50 @@ class Tracker:
                     except Exception as e:
                         print(e)
                         print("received unparsable data")
+
+    def listen_tcp(self):
+        while 1:
+            read, write, ex = select.select(self.read_tcp, self.write_tcp, [])
+            for sock in read:
+                if sock == self.server_sock2:
+                    conn, addr = self.server_sock2.accept()
+                    conn.settimeout(5.0)
+                    read.append(conn)
+                else:
+                    data = sock.recv(self.__BUF)
+                    if not data:
+                        break
+                    try:
+                        datacontent = data.decode()
+                    except:
+                        break
+                    if datacontent == "START":
+                        _thread.start_new_thread(self.recv_files, (sock, ))
+
+    def recv_files(self, sock):
+        data = None
+        try:
+            data = sock.recv(self.__BUF)
+        except:
+            print("file name was not received on time")
+        try:
+            datacontent = data.decode()
+            filename = datacontent
+            if filename[-8:] != ".torrent":
+                return
+            sock.send("FLOW".encode())
+            s = 0
+            length = int(pickle.loads(sock.recv(self.__BUF)))
+
+            while s != length:
+                data = sock.recv(self.__BUF)
+                s += len(data)
+                with open(f"torrents\\{filename}", "ab") as f:
+                    f.write(data)
+                sock.send("FLOW".encode())
+            sock.send("DONE".encode())
+        except:
+            return
 
     def build_announce_response(self):
         message = (1).to_bytes(4, byteorder='big')  # action - announce
